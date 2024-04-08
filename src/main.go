56d2a8c2c09c77f124881efaf8c603534b2179cd
ilhamhanif <ilhamhanif12@gmail.com>
2024-04-08 21:41:03 +0700

    create: /src add source code

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package function
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("GCSBucketNotifBQLog", GCSBucketNotifBQLog)
+}
+
+func GCSBucketNotifBQLog(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Hello World!")
+}
