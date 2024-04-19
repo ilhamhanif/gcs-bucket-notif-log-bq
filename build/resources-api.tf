989d7e59cfd9250fd0694f67c261059ce6fb2701
ilhamhanif <ilhamhanif12@gmail.com>
2024-04-19 22:16:04 +0700

    update: add cloudresourcemanager.googleapis.com api enable

--- build/resources-api.tf
+++ build/resources-api.tf
@@ -1,5 +1,12 @@
 # Enable API Services
 # Get service name from `gcloud services list --available | grep [SERVICE NAME]`
+# CLoud Resource Manager API is a MUST
+resource "google_project_service" "api_cloud_resource_manager" {
+  project            = var.project_id
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "api_artifact_registry" {
   project            = var.project_id
   service            = "artifactregistry.googleapis.com"
@@ -15,6 +22,7 @@ resource "google_project_service" "api_cloud_build" {
 # Give Delay
 resource "time_sleep" "resource_api_cloud_function_sleep" {
   depends_on = [
+    google_project_service.api_cloud_resource_manager,
     google_project_service.api_artifact_registry,
     google_project_service.api_cloud_build
   ]
