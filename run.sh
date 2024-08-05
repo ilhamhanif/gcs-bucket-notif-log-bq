0cbfab08c19748ada8a1c596ff810cb3d202749e
ilhamhanif <ilhamhanif12@gmail.com>
2024-08-06 06:46:06 +0700

    add: runner.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Initialization
 if [ "$1" == "init" ]
 then
+
     if [ "$2" == "" ]
     then
         echo "Initialize Terraform."
@@ -10,29 +12,35 @@ then
         echo "Upgrading current Terraform infrastructure state."
         terraform -chdir=build init -upgrade
     fi
+
+# Building Block
 elif [ "$1" == "build" ]
 then
-    if [ "$3" == "" ]
+
+    if [ "$3" != "" ]
     then
-        echo "Please state environment variable .tfvars file name (without extension)"
-        exit 1
-    else
         envVariableFile=$3.tfvars
+    else # Error Handler
+        echo "Please state environment variable .tfvars file name (without extension)."
+        exit 1
     fi
 
-    if [ "$2" == "sync" ]
+    if [ "$2" == "sync" ] # Sync
     then
         echo "Syncing current state infrastructure with target."
         terraform -chdir=build apply -var-file $envVariableFile -auto-approve -refresh-only
-    elif [ "$2" == "deploy" ]
+    elif [ "$2" == "deploy" ] # Deploy
     then
         echo "Deploying infrastructure."
         terraform -chdir=build apply -var-file $envVariableFile -auto-approve
-    else
-        echo "Please define command [sync/deploy]"
+    else # Error Handler
+        echo "Please define command [sync/deploy]."
         exit 1
     fi
-else
-    echo "Please define command [init/build]"
+
+else # Handler
+
+    echo "Please define command [init/build]."
     exit 1
+
 fi
